dc54dc350731ad82b84783ed89c0dc46a95f9964
Koizumi-Tetsuro <24ec056@ms.dendai.ac.jp>
2026-06-07 19:37:18 +0900

    Update forPhone.js

--- forPhone.js
+++ forPhone.js
@@ -180,8 +180,8 @@ function showShift() {
 window.addEventListener("keydown", event => {
     let focusNow = document.activeElement.id;
 
-    // Enterキー押下時
-    if (event.key == "Enter") {
+    // Enterキー，下矢印押下時
+    if (event.key == "Enter" || event.key == "ArrowDown") {
         if (focusNow.includes("Start")) {
             if (document.getElementById(focusNow).value == "") {
                 focusNow = Number(focusNow.replace("Start", "")) + 1 + "Start";
@@ -198,45 +198,19 @@ window.addEventListener("keydown", event => {
                 showShift();
             }
         }
+    }
+
+    // 上矢印押下時
+    if (event.key == "ArrowUp" && !focusNow.includes(half)) {
+        if (focusNow.includes("Start")) {
+            focusNow = Number(focusNow.replace("Start", "")) - 1 + "Start";
+        } else if (focusNow.includes("End")) {
+            focusNow = Number(focusNow.replace("End", "")) - 1 + "End";
+        }
+    }
+
+    // Enterキー，矢印キー押下で対応する入力欄へフォーカス
+    if (event.key == "Enter" || event.key.includes("Arrow")) {
         document.getElementById(focusNow).focus();
     }
 });
-
-        // 入力欄のフォーカス遷移
-        window.addEventListener("keydown", event => {
-            let focusNow = document.activeElement.id;
-
-            // Enterキー，下矢印押下時
-            if (event.key == "Enter" || event.key == "ArrowDown") {
-                if (focusNow.includes("Start")) {
-                    if (document.getElementById(focusNow).value == "") {
-                        focusNow = Number(focusNow.replace("Start", "")) + 1 + "Start";
-                    } else {
-                        focusNow = focusNow.replace("Start", "") + "End"
-                    }
-                } else if (focusNow.includes("End")) {
-                    focusNow = Number(focusNow.replace("End", "")) + 1 + "Start";
-                }
-
-                if (focusNow.includes(lastdate.getDate() + 1)) {
-                    focusNow = generate.id;
-                    if (event.key !== "ArrowRight") {
-                        showShift();
-                    }
-                }
-            }
-
-            // 上矢印押下時
-            if (event.key == "ArrowUp" && !focusNow.includes(half)) {
-                if (focusNow.includes("Start")) {
-                    focusNow = Number(focusNow.replace("Start", "")) - 1 + "Start";
-                } else if (focusNow.includes("End")) {
-                    focusNow = Number(focusNow.replace("End", "")) - 1 + "End";
-                }
-            }
-
-            // Enterキー，矢印キー押下で対応する入力欄へフォーカス
-            if (event.key == "Enter" || event.key.includes("Arrow")) {
-                document.getElementById(focusNow).focus();
-            }
-        });
